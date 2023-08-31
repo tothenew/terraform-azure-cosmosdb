15bf0fbeae7dbc06a4d821cc0f0a8aa7c94dbeb0
GauravNegi <gaurav1@tothenew.com>
2023-08-31 16:44:25 +0530

    update source

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,12 @@
 module "cosmosdb" {
-  source                          = "git::https://github.com/gauravnegi01/terraform-azure-cosmosdb.git?ref=cosmosdb-v1"
+  source                          = "git::https://github.com/tothenew/terraform-azure-cosmosdb.git"
 
   create_mongodb                  = false
   create_postgresql               = false
   create_cassandra                = false
   create_table                    = false
   create_sql                      = false
-  create_gremlin                  = true
+  create_gremlin                  = false
   
   project_name_prefix             = "Azure_cosmmosdb"
 
@@ -18,6 +18,7 @@ module "cosmosdb" {
 
   # set public_network_access_enabled = true if you want to enable public access and add allow public in firewall rule
   # by default public_network_access_enabled is false
+
   # public_network_access_enabled    = true  
   # allowed_cidrs                   = ["61.12.91.218"]
 
